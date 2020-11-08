7af801c11af84193c299386c3f0e73b807ddf49f
AMcCall22 <alisonlmccallum@gmail.com>
2020-11-08 21:56:08 +0000

    added shopping bag view, urls and template

--- shopping_bag/contexts.py
+++ shopping_bag/contexts.py
@@ -1,9 +1,16 @@
 def shopping_bag_contents(request):
 
-    shopping_bag = []
+    shopping_bag_items = []
     total = 0
     product_count = 0
 
-    context = {}
+    grand_total = total
+
+    context = {
+        'shopping_bag_items': shopping_bag_items,
+        'total': total,
+        'product_count': product_count,
+        'grand_total': grand_total,
+    }
 
     return context
