72c9b42d18432840e745dbd3ec9d9c5cba3c256b
AMcCall22 <alisonlmccallum@gmail.com>
2020-11-12 14:19:00 +0000

    checkout form created

--- checkout/apps.py
+++ checkout/apps.py
@@ -1,5 +1,8 @@
 from django.apps import AppConfig
 
+"""
+Code borrowed from CI's Boutique Ado project, Admin, Signals & Forms Part 1. 
+"""
 
 class CheckoutConfig(AppConfig):
     name = 'checkout'
