b3be3d5b368bc1e9e62ae0867ef0dd0f429a2090
AMcCall22 <alisonlmccallum@gmail.com>
2020-11-12 10:56:53 +0000

    shopping bag logic fixed

--- shopping_bag/contexts.py
+++ shopping_bag/contexts.py
@@ -6,37 +6,26 @@ def shopping_bag_contents(request):
 
     shopping_bag_items = []
     total = 0
+    product_count = 0
     bean_count = 0
     shopping_bag = request.session.get('shopping_bag', {})
 
-    for item_id, item_data in shopping_bag.items():
-        if isinstance(item_data, int):
-            bean = get_object_or_404(Bean, pk=item_id)
-            total += item_data * bean.price
-            bean_count += item_data
-            shopping_bag_items.append({
-                'item_id': item_id,
-                'quantity': item_data,
-                'bean': bean,
-            })
-
-        else:
-            bean = get_object_or_404(Bean, pk=item_id)
-            for sizes, quantity in item_data['items_by_size'].items():
-                total += quantity * bean.price
-                bean_count += quantity
-                shopping_bag_items.append({
-                    'item_id': item_id,
-                    'quantity': quantity,
-                    'bean': bean,
-                    'size': sizes,
-                })
+    for item_id, quantity in shopping_bag.items():
+        bean = get_object_or_404(Bean, pk=item_id)
+        total += quantity * bean.price
+        bean_count += quantity
+        shopping_bag_items.append({
+            'item_id': item_id,
+            'quantity': quantity,
+            'bean': bean,
+        })
 
     grand_total = total
 
     context = {
         'shopping_bag_items': shopping_bag_items,
         'total': total,
+        'product_count': product_count,
         'bean_count': bean_count,
         'grand_total': grand_total,
     }
--- shopping_bag/views.py
+++ shopping_bag/views.py
@@ -1,29 +1,18 @@
-from django.shortcuts import render
 from django.shortcuts import render, redirect
-
 # Create your views here.
-
-
 def view_shopping_bag(request):
-    """A view to return the index page"""
     """ A view that renders the bag contents page """
-
     return render(request, 'shopping_bag/shopping_bag.html')
-
-
 def add_to_shopping_bag(request, item_id):
     """ Add a quantity of the specified product to the shopping bag """
-
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     shopping_bag = request.session.get('shopping_bag', {})
-
     if item_id in list(shopping_bag.keys()):
         shopping_bag[item_id] += quantity
     else:
         shopping_bag[item_id] = quantity
 
-    return render(request, 'shopping_bag/shopping_bag.html') 
     request.session['shopping_bag'] = shopping_bag
     print(request.session['shopping_bag'])
     return redirect(redirect_url)
