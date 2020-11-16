7c9297ccf9447c47c7d7108951659b73cb24be01
AMcCall22 <alisonlmccallum@gmail.com>
2020-11-16 13:19:53 +0000

    procfile and settings.py updated

--- coffee_options/settings.py
+++ coffee_options/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'i+ab#+pyz0-l-9w=7ykh#re-uocd)(k$vg%w%+$e^i3v@*k!m4'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['coffee-choices.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -119,17 +119,21 @@ WSGI_APPLICATION = 'coffee_options.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
-
-
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
+
 
 AUTH_PASSWORD_VALIDATORS = [
     {
