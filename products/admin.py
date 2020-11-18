b726c0b870c03b5e4c7666b5c1b5760a14f74fe8
AMcCall22 <alisonlmccallum@gmail.com>
2020-11-18 15:30:33 +0000

    fixtures uploaded

--- products/admin.py
+++ products/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Bean
+from .models import Bean, Country
 
 # Register your models here.
 admin.site.register(Bean)
+admin.site.register(Country)
--- products/models.py
+++ products/models.py
@@ -1,16 +1,26 @@
 from django.db import models
 
 
+class Country(models.Model):
+
+    name = models.CharField(max_length=254)
+    friendly_name = models.CharField(max_length=254, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_friendly_name(self):
+        return self.friendly_name
+
+
 class Bean(models.Model):
-    country = models.CharField(max_length=254)
+    country = models.ForeignKey('Country', null=True, blank=True, on_delete=models.SET_NULL)
+    bag_size = models.CharField(max_length=254)
     description = models.TextField()
     sizes = models.BooleanField(default=False, null=True)
-    small_price = models.DecimalField(max_digits=6, decimal_places=2, default=5)
-    large_price = models.DecimalField(max_digits=6, decimal_places=2, default=7.5)
     price = models.DecimalField(max_digits=6, decimal_places=2, default=0)
     strength = models.CharField(max_length=5)
-    image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField()
 
     def __str__(self):
-        return self.country
+        return self.bag_size
