3281b56bd9b2d05315abb3a40addbea66275455c
AMcCall22 <alisonlmccallum@gmail.com>
2020-11-19 14:19:34 +0000

    beans_detail basic styling

--- products/models.py
+++ products/models.py
@@ -6,6 +6,7 @@ class Country(models.Model):
     name = models.CharField(max_length=254)
     friendly_name = models.CharField(max_length=254, null=True, blank=True)
     image = models.ImageField(default=False)
+    description = models.TextField(default=False)
 
     def __str__(self):
         return self.name
@@ -17,10 +18,10 @@ class Country(models.Model):
 class Bean(models.Model):
     country = models.ForeignKey('Country', null=True, blank=True, on_delete=models.SET_NULL)
     bag_size = models.CharField(max_length=254)
-    description = models.TextField()
     sizes = models.BooleanField(default=False, null=True)
     price = models.DecimalField(max_digits=6, decimal_places=2, default=0)
     strength = models.CharField(max_length=5)
+    friendly_name = models.CharField(max_length=254, null=True, blank=True)
 
     def __str__(self):
         return self.bag_size
