0ed362e29f51b5da38d8b408f91b512de08725b4
AMcCall22 <alisonlmccallum@gmail.com>
2020-11-20 14:51:19 +0000

    profile order history template and functionality added

--- checkout/forms.py
+++ checkout/forms.py
@@ -26,7 +26,6 @@ class OrderForm(forms.ModelForm):
 
             self.fields['full_name'].widget.attrs['autofocus'] = True
             for field in self.fields:
-
                 placeholder = placeholders[field]
             self.fields[field].widget.attrs['placeholder'] = placeholder
             self.fields[field].widget.attrs['class'] = 'stripe-style-input'
