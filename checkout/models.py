0926a454a41c47592f6328eb086e441663096add
AMcCall22 <alisonlmccallum@gmail.com>
2020-11-20 11:22:06 +0000

    profiles model created

--- checkout/models.py
+++ checkout/models.py
@@ -4,6 +4,7 @@ from django.db import models
 from django.db.models import Sum
 
 from products.models import Bean
+from profiles.models import UserProfile
 
 from django_countries.fields import CountryField
 
@@ -14,6 +15,7 @@ Code borrowed from CI's Boutique Ado project, Models Part 2
 
 class Order(models.Model):
     order_number = models.CharField(max_length=32, null=False, editable=False)
+    user_profile = models.ForeignKey(UserProfile, on_delete=models.SET_NULL, null=True, blank=True, related_name='orders')
     full_name = models.CharField(max_length=50, null=False, blank=False)
     street_address1 = models.CharField(max_length=80, null=False, blank=False)
     street_address2 = models.CharField(max_length=80, null=True, blank=True)
