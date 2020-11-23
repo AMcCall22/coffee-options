d69010a3badc18b686a90321415000eeb76ca98a
AMcCall22 <alisonlmccallum@gmail.com>
2020-11-23 18:03:34 +0000

    blog detail views and basic templates set up

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from . import views
+# from . import views
+from .views import BlogView, BlogDetail
 
 urlpatterns = [
-    path('', views.blog, name='blog'),
+    path('', BlogView.as_view(), name="blog"),
+    path('blog_detail/<int:pk>', BlogDetail.as_view(), name="blog-detail"),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from .models import Post
 
 
-def blog(request):
-    return render(request, 'blog.html', {})
+# def blog(request):
+#     return render(request, 'blog.html', {})
+
+class BlogView(ListView):
+    model = Post
+    template_name = 'blog.html'
+
+
+class BlogDetail(DetailView):
+    model = Post
+    template_name = 'blog_details.html'
