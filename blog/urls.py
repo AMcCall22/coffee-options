a86d9a74c3982c2c8a8035df23f64501874a85a6
AMcCall22 <alisonlmccallum@gmail.com>
2020-11-24 11:29:12 +0000

    like button and functionality added

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 # from . import views
-from .views import BlogView, BlogDetail, AddBlogPost
+from .views import BlogView, BlogDetail, AddBlogPost, LikeView
 
 urlpatterns = [
     path('', BlogView.as_view(), name="blog"),
     path('blog_detail/<int:pk>', BlogDetail.as_view(), name="blog-detail"),
-    path('add_post/', AddBlogPost.as_view(), name="add-blog-post")
+    path('add_post/', AddBlogPost.as_view(), name="add-blog-post"),
+    path('likes/<int:pk>', LikeView, name="like-post")
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,9 @@
 from django.views.generic import ListView, DetailView, CreateView
 from .models import Post
+from django.shortcuts import get_object_or_404
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+
 
 # Codemy
 
@@ -12,8 +16,22 @@ class BlogDetail(DetailView):
     model = Post
     template_name = 'blog_details.html'
 
+    def get_context_data(self, *args, **kwargs):
+        context = super(BlogDetail, self).get_context_data(*args, **kwargs)
+        data = get_object_or_404(Post, id=self.kwargs['pk'])
+        total_likes = data.total_likes()
+        context['total_likes'] = total_likes
+        return context
+
 class AddBlogPost(CreateView):
     model = Post
     template_name = 'add_post.html'
     fields = '__all__'
+
+def LikeView(request, pk):
+    post = get_object_or_404(Post, id=request.POST.get('post_id'))
+    post.likes.add(request.user)
+    return HttpResponseRedirect(reverse('blog-detail', args=[str(pk)]))
+
+
 
