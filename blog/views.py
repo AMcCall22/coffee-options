3c77a60cfff5ff64ea267631777832f0e061bf1b
AMcCall22 <alisonlmccallum@gmail.com>
2020-11-26 21:10:58 +0000

    checkout static files beautified

--- blog/views.py
+++ blog/views.py
@@ -1,18 +1,22 @@
-from django.views.generic import ListView, DetailView, CreateView
+from django.views.generic import ListView, DetailView
 from .models import Post
 from django.shortcuts import get_object_or_404
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 
 
-# Codemy
+# A tutorial from Codemy was followed to provide
+# a basis for the blog functionality
+# https://www.youtube.com/watch?v=B40bteAMM_M
 
 class BlogView(ListView):
+    """ A view to show a list of all blog entries """
     model = Post
     template_name = 'blog.html'
 
 
 class BlogDetail(DetailView):
+    """ A view to show the detail of a blog entry """
     model = Post
     template_name = 'blog_details.html'
 
@@ -23,15 +27,9 @@ class BlogDetail(DetailView):
         context['total_likes'] = total_likes
         return context
 
-class AddBlogPost(CreateView):
-    model = Post
-    template_name = 'add_post.html'
-    fields = '__all__'
 
 def LikeView(request, pk):
+    """ A view to allow a website user to like a blog post """
     post = get_object_or_404(Post, id=request.POST.get('post_id'))
     post.likes.add(request.user)
     return HttpResponseRedirect(reverse('blog-detail', args=[str(pk)]))
-
-
-
