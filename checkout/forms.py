c4b51bf4bd4ff934d9298a3ea9a7bd74ca62f9e2
AMcCall22 <alisonlmccallum@gmail.com>
2020-11-28 19:54:29 +0000

    forms.py updated in checkout

--- checkout/forms.py
+++ checkout/forms.py
@@ -8,3 +8,26 @@ class OrderForm(forms.ModelForm):
         fields = ('full_name', 'street_address1', 'street_address2',
                   'town_or_city', 'postcode', 'country', 'email',
                   'phone_number')
+
+    def __init__(self, *args, **kwargs):
+        """
+        Add placeholders and classes, remove auto-generated
+        labels and set autofocus on first field
+        """
+        super().__init__(*args, **kwargs)
+        placeholders = {
+            'full_name': 'Full Name',
+            'street_address1': 'Street Address 1',
+            'street_address2': 'Street Address 2',
+            'postcode': 'Postal Code',
+            'town_or_city': 'Town or City',
+            'country': 'Country',
+            'email': 'Email Address',
+            'phone_number': 'Phone Number',
+            }
+
+        self.fields['full_name'].widget.attrs['autofocus'] = True
+        for field in self.fields:
+            placeholder = placeholders[field]
+            self.fields[field].widget.attrs['placeholder'] = placeholder
+            self.fields[field].widget.attrs['class'] = 'stripe-style-input'
